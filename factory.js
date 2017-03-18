f1faa5201aef374304907d0d7b9f93c3ea2a97c7
Saurav Tiru <sauravezio@gmail.com>
2017-03-18 22:23:55 +0530

    added tooltip functionalities

--- factory.js
+++ factory.js
@@ -2,7 +2,7 @@
 function designModeOn(){
 
   console.log("Thanks for viewing the console! #LetsDesign")
-  storyArea.document.designMode = 'On';
+//  storyArea.document.designMode = 'On';
 
 }
 
@@ -12,6 +12,7 @@ console.log(storyArea);
 console.log(document.getElementById('storyArea'));
 function execCmd(command){
  console.log('bold');
+ console.log(command);
   document.execCommand(command,false,null);
 }
 
@@ -23,26 +24,31 @@ document.execCommand(command,false,'#FF0000');
 var bt1 = document.getElementById('btnX');
 var bt2 = document.getElementById('btnY');
 var bt3 = document.getElementById('btnZ');
-
+var btnX = document.getElementById('exit');
 
 bt1.addEventListener('click', function (event) {
     execCmd('bold');
-    $("#tooltip").hide();
+    $("#tooltip").hide('1000');
 });
 bt2.addEventListener('click', function (event) {
     execCmd('underline');
-    $("#tooltip").hide();
+    $("#tooltip").hide('1000');
 });
 bt3.addEventListener('click', function (event) {
-    execCmd('forecolor');
-    $("#tooltip").hide();
+    execCmd2('forecolor');
+    $("#tooltip").hide('1000');
 });
+btnX.addEventListener('click',function(event){
+   $("#tooltip").hide('1000');
+
+});
+
 
 
 
 $(document).ready(function () {
     $("#tooltip").hide();
-    $('body').mouseup(function (e) {
+    $('#storyArea').mouseup(function (e) {
         var x = e.clientX;
         var y = e.clientY;
         placeTooltip(x, y);
