89a8af58980d1699a543e94928cdf389c15b29c8
Saurav Tiru <sauravezio@gmail.com>
2017-03-18 15:02:54 +0530

    functionalities added

--- factory.js
+++ factory.js
@@ -1,5 +1,16 @@
 function designModeOn(){
   console.log("Thanks for viewing the console! #LetsDesign")
   storyArea.document.designMode = 'On';
-  
+
+}
+
+
+function execCmd(command){
+
+  storyArea.document.execCommand(command,false,null);
+}
+
+function execCmd2(command){
+
+  storyArea.document.execCommand(command,false,'#FF0000');
 }
