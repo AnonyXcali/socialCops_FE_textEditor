b40c5d95cc018b7555c1ea950f44e5f73ad219e3
Saurav Tiru <sauravezio@gmail.com>
2017-03-19 01:42:42 +0530

    added anchor tag functionality and much more

--- factory.js
+++ factory.js
@@ -1,11 +1,22 @@
 
+var bt1 = document.getElementById('btnX');
+var bt2 = document.getElementById('btnY');
+var bt3 = document.getElementById('btnZ');
+var btnX = document.getElementById('exit');
+var linkX = document.getElementById('link');
+var submit = document.getElementById('submit');
+var textArea = document.getElementById('storyArea');
+var linkCounter = 0;
+
+
+
+
 function designModeOn(){
 
   console.log("Thanks for viewing the console! #LetsDesign")
 //  storyArea.document.designMode = 'On';
 
 }
-
 
 
 console.log(storyArea);
@@ -21,10 +32,6 @@ function execCmd2(command){
 document.execCommand(command,false,'#FF0000');
 }
 
-var bt1 = document.getElementById('btnX');
-var bt2 = document.getElementById('btnY');
-var bt3 = document.getElementById('btnZ');
-var btnX = document.getElementById('exit');
 
 bt1.addEventListener('click', function (event) {
     execCmd('bold');
@@ -42,20 +49,37 @@ btnX.addEventListener('click',function(event){
    $("#tooltip").hide('1000');
 
 });
+linkX.addEventListener('click',function(event){
+  urlLinkCreator();
+   $("#tooltip").hide('1000');
+
+});
+
+submit.addEventListener('click',function(event){
+console.log(textArea.innerHTML.trim());
+
+});
+// $('body').click(function(){
+//   $("#tooltip").hide('1000');
+// });
+
 
 
 
 
 $(document).ready(function () {
     $("#tooltip").hide();
-    $('#storyArea').mouseup(function (e) {
+    $('#storyArea').mouseup().mousedown().mouseup().mousedown(function (e) {
         var x = e.clientX;
         var y = e.clientY;
         placeTooltip(x, y);
         $("#tooltip").show('1000');
 
     })
+
 });
+
+
 
 
 
@@ -71,3 +95,31 @@ $(function () {
 
     $(document).tooltip();
 });
+
+$( ".container" ).mouseleave(function() {
+      $("#tooltip").hide('20');
+
+});
+
+
+storyArea.addEventListener('keypress', function(e) {
+    if (e.keyCode == '13') {
+        e.preventDefault();
+        document.execCommand('insertParagraph', false);
+        document.execCommand('formatBlock', false, 'p');
+    }
+}, false);
+
+
+
+function urlLinkCreator() {
+    var linkURL = prompt("URL:", "http://");
+    if (linkURL !== null) {
+    	document.execCommand("CreateLink", false, linkURL);
+	    if (linkCounter % 2 == 0)
+	        $('<li> <a href="' + linkURL + '">' + linkURL + '</a></li>').appendTo('#links');
+	    else
+	        $('<li> <a style="color:red;" href="' + linkURL + '">' + linkURL + '</a></li>').appendTo('#links');
+	    linkCounter += 1;
+	}
+}
